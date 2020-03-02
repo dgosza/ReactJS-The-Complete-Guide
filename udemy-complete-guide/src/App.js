7d2d1b200da90941639b3e4cb7d47287ce17f9a9
Diego Souza <diegomagasouza@gmail.com>
2020-03-01 23:36:05 -0300

    update files

--- udemy-complete-guide/src/App.js
+++ udemy-complete-guide/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-   return (
-      <div className="App">
-         <h1>Hi! I'm a React App!</h1>
-      </div>
-   );
+class App extends Component{
+   render(){
+      return(
+         <div className="App">
+            <h1>oi</h1>
+            <h1>oi</h1>
+         </div>
+      )
+   }
 }
 
 export default App;
