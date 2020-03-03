ce9cadbbc282279c57bb42b5e212b8613e8f81a1
Diego <diegomagasouza@gmail.com>
2020-03-03 16:10:37 -0300

    estudo react

--- udemy-complete-guide/src/App.js
+++ udemy-complete-guide/src/App.js
@@ -10,47 +10,79 @@ const App = () => {
          { name: 'Manu', age: 29 },
          { name: 'Ste', age: 30 }
       ],
-      otherState: 'AnotherState',
-      counter: 0
+      otherState: 'AnotherState'
    });
 
-   console.log(personsState)
-
-
-   const switchNameHandler = () => {
+   const switchNameHandler = (newName) => {
       // console.log('entrei no button hehe')
-      //DONT DO THIS: personsState.persons[0].name = 'Diego Fulano de Tal';
-   
+      //DONT DO THIS: personsState.persons[0].name = 'Diego Fulano de Tal'
       setPersonsState({
          persons: [
-            { name: 'Diego Magalhaes de Souza', age: 999 },
+            { name: newName, age: 999 },
             { name: 'de Souza', age: 122 },
             { name: 'Magalhaes', age: 12 }
-         ]
+         ],
+         otherState: personsState.otherState,
       })
    }
-   
+
+   const nameChangedHandler = (event) =>{
+      setPersonsState({
+         persons: [
+            { name: 'Maxxxxx', age: 999 },
+            { name: event.target.value, age: 122 },
+            { name: 'Souza Magalhaes', age: 12 }
+         ],
+         otherState: personsState.otherState,
+      })
+   }
+
+   const [counter, setCounter] = useState({ counter: 0 })
+   const addCounter = () => {
+      setCounter({
+         counter: counter.counter + 1
+      })
+   }
+
+
 
    return (
       <div className="App">
          <h1>React App</h1>
          <h1>learning</h1>
 
-         <Button variant="contained" color="secondary" onClick={switchNameHandler}>
+         <Button variant="contained" color="secondary" onClick={() => switchNameHandler('Trocando nome by arrowfunction')}>
             click here
-            </Button>
+         </Button>
 
-         <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>
+         <Button variant="contained" color="secondary" onClick={addCounter}>
+            click here to change the counter value, ++1
+         </Button>
+
+         <Person
+            name={personsState.persons[0].name}
+            age={personsState.persons[0].age}
+         >
             This is a props children
-            </Person>
+         </Person>
 
-         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
+         <Person
+            name={personsState.persons[1].name}
+            age={personsState.persons[1].age}
+            click={switchNameHandler.bind(this, 'maximilian be')} //passando como argumento pra function.
+            changed={nameChangedHandler}
+         >
             My Hobbies: Racing
-            </Person>
+         </Person>
 
-         <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
+         <Person
+            name={personsState.persons[2].name}
+            age={personsState.persons[2].age}
+         >
             inside the person component. we access the value using props.children
-            </Person>
+         </Person>
+
+         <Person name={counter.counter}></Person>
 
       </div>
    )
--- udemy-complete-guide/src/Person/Person.js
+++ udemy-complete-guide/src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+
 const Person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old</p>
+        <div className="person">
+            <p onClick={props.click} >I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
+            <p>{props.counter}</p>
+            <input type="text" onChange={props.changed}/>
         </div>
     )
 };
