374b15f6c55ad151ee9c0a418ee7847bbf9aee68
Diego Souza <diegomagasouza@gmail.com>
2020-03-07 19:56:33 -0300

    learning material ui grid

--- working-with-material-ui/src/App.js
+++ working-with-material-ui/src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 const App = () => {
-  return (
-    <h1>teste</h1>
-  );
+
+   // const styles = styles{
+   //    backgroundColor: 'blue'
+   // }
+
+   return (
+
+
+      <Container>
+         <Grid container spacing={3}>
+            <Grid item xs={12} md={12} ><Paper elevation={3}> xs 12</Paper></Grid>
+            <Grid item xs={6}><Paper elevation={3}> xs 6</Paper></Grid>
+            <Grid item xs={6}><Paper elevation={3}> xs 6</Paper></Grid>
+
+            <Grid item xs={3}><Paper elevation={3}> xs 3</Paper></Grid>
+            <Grid item xs={3}><Paper elevation={3}> xs 3</Paper></Grid>
+            <Grid item xs={3}><Paper elevation={3}> xs 3</Paper></Grid>
+            <Grid item xs={3}><Paper elevation={3}> xs 3</Paper></Grid>
+
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            <Grid item xs={2}><Paper elevation={3}> xs 2</Paper></Grid>
+            
+         </Grid>
+      </Container>
+
+   );
 }
 
 export default App;
