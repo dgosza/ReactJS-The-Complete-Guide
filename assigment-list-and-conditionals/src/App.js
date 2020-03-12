ea057b05e529456ff7e7a4498b56222a5d1d3268
Diego <diegomagasouza@gmail.com>
2020-03-12 16:49:53 -0300

    still doing

--- assigment-list-and-conditionals/src/App.js
+++ assigment-list-and-conditionals/src/App.js
@@ -1,42 +1,63 @@
 import React, { useState } from 'react';
 import ValidationComponent from './components/ValidationComponent'
+import Charcomponent from './components/CharComponent';
 
 import './App.css';
 
 const App = () => {
 
-  const [counter, setCounter] = useState({ value: 0 })
-  const [showCounter, setShowCounter] = useState({ value: false })
-  const [textValue, setTextValue] = useState({ value: null })
+   const [counter, setCounter] = useState({ value: 0 })
+   const [showCounter, setShowCounter] = useState({ value: false })
+   const [textValue, setTextValue] = useState({ value: null })
+   const [arraytextValue, setarrayTextValue] = useState({ value: [] })
 
-  const verifyStatus = (event) => {
-    const name = event.target.value
-    const value = name.length != 0 ? true : false;
-    setShowCounter({ value: value });
-    setTextValue({ value: name })
-    if (value) {
-      const nameLenght = name.length;
-      setCounter({ value: nameLenght })
-    }
-  }
+   const verifyStatus = (event) => {
+      const name = event.target.value
+      const nameArray = name.split("");
+      const value = name.length != 0 ? true : false;
 
-  if (showCounter.value) {
-    var contador = (
-      <div id="center">
-        CONTADOR: <ValidationComponent length={counter.value} text={textValue.value} />
+      setShowCounter({ value: value });
+      setTextValue({ value: name })
+      setarrayTextValue({ value: nameArray })
+
+      if (value) {
+         const nameLenght = name.length;
+         setCounter({ value: nameLenght })
+      }
+   }
+
+   const handleCharComponent = (event, eachLetter, index) => {
+      const texto = event.target.value;
+      console.log(texto)
+      console.log(index)
+   }
+
+   if (showCounter.value) {
+      var contador = (
+         <div id="center">
+            CONTADOR: <ValidationComponent length={counter.value} text={textValue.value} />
+         </div>
+      )
+   }
+
+   const componente = (
+      arraytextValue.value.map((eachLetter, index) => {
+         return <Charcomponent text={eachLetter} key={index} array={arraytextValue} click={ (event)=> handleCharComponent( eachLetter, index ) } ></Charcomponent>
+      })
+   )
+
+
+
+
+   return (
+      <div className="App">
+         <input placeholder="digite aqui..." onChange={verifyStatus} ></input>
+
+         {contador}
+         {componente}
+
       </div>
-    )
-  }
-
-
-  return (
-    <div className="App">
-      <input placeholder="digite aqui..." onChange={verifyStatus} ></input>
-
-      {contador}
-
-    </div>
-  );
+   );
 }
 
 export default App;
--- assigment-list-and-conditionals/src/components/ValidationComponent.js
+++ assigment-list-and-conditionals/src/components/ValidationComponent.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Charcomponent from './CharComponent';
+import React, { useState} from 'react';
+// import Charcomponent from './CharComponent';
 
 const ValidationComponent = (props) => {
-
-    const handleCharComponent = (letter, index) =>{
-        console.log(textArray[index])
-    }
+    // var textReceived = props.text;
+    // var textArray = textReceived.split("")    
     
-    const textReceived = props.text;
-    const textArray = textReceived.split("")    
+    // const handleCharComponent = (eachLetter, index) =>{
+    //     console.log(eachLetter.value)
+    //     const copyTextArray = [...textArray];
+    //     copyTextArray.splice(index, 1)
+        
+    //     textArray = copyTextArray
+    //     console.log(textArray)
 
+    // }
+    
     if (props.length <= 5) {
         var text = 'Text too short'
     } else {
@@ -21,10 +26,10 @@ const ValidationComponent = (props) => {
         <>
             {props.length}
             <p>{text}</p>
-
+{/* 
             {textArray.map( (eachLetter, index) => {
                 return <Charcomponent text={eachLetter} key={index} array={textArray} click={(event) => handleCharComponent(eachLetter, index)} ></Charcomponent>
-            } )}
+            } )} */}
         </>
     );
 }
