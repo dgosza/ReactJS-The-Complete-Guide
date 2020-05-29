30bec437b740aecb5302b36c70b505a63e20741e
dieezs <diegomagasouza@gmail.com>
2020-05-28 23:09:25 -0300

    adding lifecycle on some components

--- burgerBuilder/src/components/UI/Modal/index.js
+++ burgerBuilder/src/components/UI/Modal/index.js
@@ -4,18 +4,35 @@ import './style.css'
 import Aux from '../../../hoc/Auxiliar'
 import Backdrop from '../Backdrop'
 
-const Modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            className="Modal"
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-1000vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends React.Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        //Se o estado antigo for diferente do que o atual, entao atualiza.
+        if (nextProps.show !== this.props.show) {
+            return true
+        }
+    }
+
+    componentDidUpdate(){
+        console.log('[modal] componentDidUpdate')
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className="Modal"
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-1000vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
 
 export default Modal;
